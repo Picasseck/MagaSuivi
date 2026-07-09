d85e9eb5a418c97a59ca86120b3c7417497d13e9
Baylat <baylatseck05@gmail.com>
2026-07-09 20:27:24 +0200

    ajout des types TypeScript,des donneeset des fonctions utilitaires (#2)

---
+++ src/utils/stock.ts
@@ -0,0 +1,18 @@
+import type { Product, StockLevel } from '../types'
+
+export function getStockLevel(product: Product): StockLevel {
+  if (product.stock === 0) return 'out'
+  if (product.stock <= product.lowStockThreshold) return 'low'
+  return 'ok'
+}
+
+export function formatPrice(amount: number): string {
+  return new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(amount)
+}
+
+export function getStockAlerts(products: Product[]): Product[] {
+  return products.filter((product) => getStockLevel(product) !== 'ok')
+}
