68dc5c61a9998d93ac69198b3dc90863877d0148
Baylat <baylatseck05@gmail.com>
2026-07-09 21:30:32 +0200

    ajout du layout : barre laterale, barre du haut et navigation avec Vue Router (#3)

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="page">
+    <h1 class="section-title">Tableau de bord</h1>
+  </div>
+</template>
---
+++ src/views/Promotions.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="page">
+    <h1 class="section-title">Soldes & promotions</h1>
+  </div>
+</template>
